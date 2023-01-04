41b03d21245238aa0e68adfbfcab6aad32affbb2
Ivan Aytzhanov <aitvann@gmail.com>
2023-01-05 00:58:50 +0300

    nvim: fix competion auto brackets

--- nvim/.config/nvim/lua/auto-pairs.lua
+++ nvim/.config/nvim/lua/auto-pairs.lua
@@ -1,27 +1,22 @@
-local autopairs = require 'nvim-autopairs'
+local autopairs = require("nvim-autopairs")
 
-autopairs.setup {
-    check_ts = true,
-    ts_config = {
-        lua = { 'string', 'source' },
-        javascript = { 'string', 'template_string' },
-        java = false,
-    },
-    disable_filetype = { 'TelescopePrompt', 'spectre_panel' },
-    fast_wrap = {
-        map = '<C-p>', -- Pair
-        chars = { '{', '[', '(', '"', '\'' },
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
-        offset = 0, -- Offset from pattern match
-        end_key = '$',
-        keys = 'qwertyuiopzxcvbnmasdfghjkl',
-        check_comma = true,
-        highlight = 'PmenuSel',
-        highlight_grey = 'LineNr',
-    },
-}
-
--- cmd integration
-local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-local cmp = require 'cmp'
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
+autopairs.setup({
+	check_ts = true,
+	ts_config = {
+		lua = { "string", "source" },
+		javascript = { "string", "template_string" },
+		java = false,
+	},
+	disable_filetype = { "TelescopePrompt", "spectre_panel" },
+	fast_wrap = {
+		map = "<C-p>", -- Pair
+		chars = { "{", "[", "(", '"', "'" },
+		pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+		offset = 0, -- Offset from pattern match
+		end_key = "$",
+		keys = "qwertyuiopzxcvbnmasdfghjkl",
+		check_comma = true,
+		highlight = "PmenuSel",
+		highlight_grey = "LineNr",
+	},
+})
