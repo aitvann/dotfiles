7d0f758dede8550e39455d0423a993ca7289e9d7
Ivan Aytzhanov <aitvann@gmail.com>
2023-05-20 14:00:34 +0300

    refactor(zsh): refactor backup keys

--- packages/zsh/.config/zsh/backup-keys.sh
+++ packages/zsh/.config/zsh/backup-keys.sh
@@ -4,9 +4,12 @@
 backup-keys() {(
     set -e
 
-    echo "exporting gpg keys..."
-    # use mktmp
+    filename=keys-$(date +%Y%m%d%H%M%S)
+    scope=$(mktemp -d /tmp/backup-${filename}-XXXXXX)
+    cd $scope
     mkdir keys
+
+    echo "exporting gpg keys..."
     gpg --export --armor 3C47594A515D2C70B8EF97781AA36C4408AB6ACE > keys/aitvann.pub.asc
     gpg --export-secret-keys --armor 3C47594A515D2C70B8EF97781AA36C4408AB6ACE > keys/aitvann.priv.asc
     gpg --export-secret-subkeys --armor 3C47594A515D2C70B8EF97781AA36C4408AB6ACE > keys/aitvann.spriv.asc
@@ -18,19 +21,14 @@ backup-keys() {(
 
     echo "encrypting keys..."
     tar -cf keys.tar -C keys/ .
-    rm -r keys
-    echo -n "Password: "
-    read -s password
-    echo
-    filename=keys-$(date +%Y%m%d%H%M%S).tar.gpg
-    gpg --batch --output "$filename" --passphrase "$password" --symmetric keys.tar
-    unset "password"
-    rm -r keys.tar
+    gpg --batch --output ${filename}.tar.gpg --symmetric keys.tar
 
     echo "pushing to storage..."
     # rclone copy "$filename" storage:./keys/
-    # rm "$filename"
-    mv "$filename" ~/data/keys/
+    mv ${filename}.tar.gpg ~/data/keys/
+
+    cd -
+    rm -r $scope
 
     set +e
 )}
