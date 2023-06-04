ec038a56326cd26f0d104f150c390139765cc101
Ivan Aytzhanov <aitvann@gmail.com>
2023-06-04 12:42:39 +0300

    zsh(deep-clean): improve

--- packages/zsh/.config/zsh/deep-clean.sh
+++ packages/zsh/.config/zsh/deep-clean.sh
@@ -1,19 +1,46 @@
 #! /usr/bin/env bash
 
-# TODO: run all of it in parallel
-deep-clean() {(
-    set -e
-
+__deep-clean-docker() {(
     docker container prune -f
     docker volume prune -f
     docker image prune -af
+)}
 
-    # TODO: run in parallel
-    for f in $HOME/**/Cargo.toml; do cargo clean --manifest-path $f; done
+__deep-clean-cargo() {(
+    find . -type f -name 'Cargo.toml' | parallel cargo clean --manifest-path {}
     cargo cache -a
+)}
 
-    nix-store --gc
-    nix-store --optimise
+__deep-clean-nix() {(
+    nix-collect-garbage -d
+    nix profile wipe-history
+    nix store gc
+    nix store optimise
+)}
+
+__sudo-deep-clean-nix() {(
+    sudo nix-collect-garbage -d
+    nix profile wipe-history
+    nix store gc
+    nix store optimise
+)}
+
+deep-clean() {(
+    set -e
+
+    __deep-clean-docker &
+    __deep-clean-cargo &
+    __deep-clean-nix
+
+    set +e
+)}
+
+sudo-deep-clean() {(
+    set -e
+
+    __deep-clean-docker &
+    __deep-clean-cargo &
+    __sudo-deep-clean-nix
 
     set +e
 )}
--- users/aitvann.nix
+++ users/aitvann.nix
@@ -78,6 +78,7 @@ in {
     postgresql_14
     syncplay-nogui
     aerc
+    parallel
 
     comma
 
