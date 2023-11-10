a0702dd59cd744c0aaec5c55bd80bac1843ee859
Ivan Aytzhanov <aitvann@gmail.com>
2023-11-10 23:31:24 +0300

    nixos: add nix-alien

--- hosts/mars/flake.nix
+++ hosts/mars/flake.nix
@@ -5,26 +5,32 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     ...
   }: {
     nixosConfigurations = {
-      mars = nixpkgs.lib.nixosSystem {
+      mars = let
         system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = false;
-            home-manager.useUserPackages = true;
-            home-manager.users.aitvann = ../../users/aitvann.nix;
-          }
-        ];
-      };
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = false;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = {inherit self system;};
+              home-manager.users.aitvann = ../../users/aitvann.nix;
+            }
+          ];
+        };
     };
   };
 }
