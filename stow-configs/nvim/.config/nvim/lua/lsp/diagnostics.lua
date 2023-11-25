78871bf9ee3432f4ed7b48a1ee8e7a5588f0637e
Ivan Aytzhanov <aitvann@gmail.com>
2023-11-25 12:04:21 +0300

    nvim: fix doc

--- stow-configs/nvim/.config/nvim/lua/lsp/diagnostics.lua
+++ stow-configs/nvim/.config/nvim/lua/lsp/diagnostics.lua
@@ -14,8 +14,8 @@ M.on_attach = function(_)
         { silent = true, desc = 'show PROJECT diagnostics (Messages)' })
     vim.keymap.set("n", '<leader>m', vim.diagnostic.open_float,
         { silent = true, desc = 'show CURRENT LINE diagnostics (Messages)' })
-    vim.keymap.set("n", '[d', nndiag.goto_prev(), { silent = true, desc = 'GOTO NEXT diagnostics' })
-    vim.keymap.set("n", ']d', nndiag.goto_next(), { silent = true, desc = 'GOTO PREVIOUS diagnostics' })
+    vim.keymap.set("n", '[d', nndiag.goto_prev(), { silent = true, desc = 'GOTO PREVIOUS diagnostics' })
+    vim.keymap.set("n", ']d', nndiag.goto_next(), { silent = true, desc = 'GOTO NEXT diagnostics' })
     -- stylua: ignore end
 
     -- diagnostics in line number
