9b5749ca4657a38d9fef162ce111354ea98933fe
Ivan Aytzhanov <aitvann@gmail.com>
2024-01-13 16:12:52 +0300

    nixos: update

--- stow-configs/nvim/.config/nvim/lua/plugin-treesitter.lua
+++ stow-configs/nvim/.config/nvim/lua/plugin-treesitter.lua
@@ -16,14 +16,6 @@ configs.setup({
         additional_vim_regex_highlighting = false,
     },
     indent = { enable = true, disable = { "yaml" } },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
     textobjects = {
         select = {
             enable = true,
@@ -146,3 +138,35 @@ context_commentstring.setup {
     enable = true,
     enable_autocmd = false,
 }
+
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+-- https://github.com/lukas-reineke/indent-blankline.nvim/issues/686#issuecomment-1745902526
+-- https://codeberg.org/dnkl/foot/issues/828#issuecomment-284301
+require("ibl").setup {
+    scope = { highlight = highlight },
+    indent = { char = "▏" },
+}
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
