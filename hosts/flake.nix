59c156870281954fe8018b2e49d4d22104395ad2
Ivan Aytzhanov <aitvann@gmail.com>
2024-02-13 11:35:32 +0300

    refactor(nixos): restructure flake.nix

--- hosts/flake.nix
+++ hosts/flake.nix
@@ -7,6 +7,8 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-alien.url = "github:thiagokokada/nix-alien";
     nix-alien.inputs.nixpkgs.follows = "nixpkgs";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,8 +22,10 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
+    deploy-rs,
     ...
   } @ inputs: {
     nixosConfigurations = {
@@ -29,16 +33,35 @@
         system = "x86_64-linux";
         specialArgs = {inherit inputs;};
         modules = [
-          ./configuration.nix
+          ./mars/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = false;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = {inherit inputs;};
-            home-manager.users.aitvann = ../../users/aitvann.nix;
+            home-manager.users.aitvann = import "${self}/../users/aitvann@mars.nix";
           }
         ];
       };
+
+      venus = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./venus/configuration.nix
+        ];
+      };
+
+      deploy.nodes.venus = {
+        hostname = "45.11.181.163";
+        sshUser = "nixos";
+        profiles.system = {
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.venus;
+        };
+      };
+
+      # This is highly advised, and will prevent many possible mistakes
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
   };
 }
