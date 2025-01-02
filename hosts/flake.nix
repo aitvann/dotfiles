a7d4cfa44d49ee698f5d5227ea06d19581ab17e8
Ivan Aytzhanov <aitvann@gmail.com>
2025-01-02 17:17:18 +0300

    feat(nixos): clean flake.lock

--- hosts/flake.nix
+++ hosts/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
+    nur.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-alien.url = "github:thiagokokada/nix-alien";
