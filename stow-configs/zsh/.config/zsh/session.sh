23b05568e28c67571afcab9eec547e69b1a53766
Ivan Aytzhanov <aitvann@gmail.com>
2025-03-23 19:49:38 +0300

    update

--- stow-configs/zsh/.config/zsh/session.sh
+++ stow-configs/zsh/.config/zsh/session.sh
@@ -38,10 +38,6 @@ alias spass="PASSWORD_STORE_DIR=${XDG_DATA_HOME}/shadow-password-store pass"
 alias restic-jupiter="restic --repo 'sftp:jupiter:/mnt/backup-storage' --password-command 'pass jupiter-backup'"
 alias nnn="n"
 alias N='sudo -E nnn -dH'
-# `browser.places.importBookmarksHTML` setting overrides existing bookmarks
-# run it in case of closing Firefox without importing bookmarks
-# TODO: move to devshell
-alias preserve-ff-bookmarks="rm ~/.local/share/firefox/bookmarks.html"
 # consider nix --extra-experimental-features repl-flake repl ".#nixosConfigurations.\"$NAME\""
 # https://discourse.nixos.org/t/use-repl-to-inspect-a-flake/28275
 alias nix-repl="nix repl nixpkgs"
