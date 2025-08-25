edddd251b47cd6fec91dcf4511f9b5caea2528e4
Ivan Aytzhanov <aitvann@gmail.com>
2025-08-25 12:19:59 +0300

    chore(nixos): update shell format

--- flake.nix
+++ flake.nix
@@ -199,7 +199,7 @@
     # This is highly advised, and will prevent many possible mistakes
     checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
-    devShell.${system} = let
+    devShells.${system}.default = let
       overlays = [nur.overlays.default];
       pkgs = import nixpkgs {inherit system overlays;};
     in
