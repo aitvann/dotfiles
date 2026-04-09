21c3dfe52328f2cdf054b6a3b2832f2019ee2349
Ivan Aytzhanov <aitvann@gmail.com>
2026-04-09 16:39:59 +0300

    feat(nvim): fix NeoVim library

--- stow-home/nvim/.config/nvim/lsp/lua_ls.lua
+++ stow-home/nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,17 +1,60 @@
+local vfn = vim.fn
+
+local library = {}
+local function add(lib)
+    for _, p in pairs(vfn.expand(lib, false, true)) do
+        local uv = vim.uv or vim.loop
+        p = uv.fs_realpath(p)
+        if p then
+            library[p] = true
+        end
+    end
+end
+-- add runtime
+-- add plugins it may be very slow to add all in path
+add('$VIMRUNTIME')
+-- add your config
+-- local home = vfn.expand("$HOME")
+add(vfn.stdpath('config'))
+
+library[vfn.expand('$VIMRUNTIME/lua')] = true
+library[vfn.expand('$VIMRUNTIME')] = true
+library[vfn.expand('$VIMRUNTIME/lua/vim')] = true
+library[vfn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+
 return {
     settings = {
-        -- NeoVim-specific settings should be in `.luarc.json` file
         Lua = {
-            workspace = {
-                -- TODO: figure out how to put it it `.luarc.json` file
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
             },
-            -- Do not send telemetry data containing a randomized but unique identifier
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim', 'describe', 'it', 'before_each', 'after_each', 'teardown', 'pending' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = library,
+                checkThirdParty = false,
+                maxPreload = 1000,
+                preloadFileSize = 40000,
+            },
             telemetry = {
-                enable = false,
+                enable = false
             },
         },
     },
+    on_new_config = function(cfg, root)
+        local libs = vim.schedule(function()
+            vim.tbl_deep_extend('force', {}, library)
+        end)
+        libs[root] = nil
+        cfg.settings.Lua.workspace.library = libs
+        return cfg
+    end,
 }
