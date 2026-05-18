a502966b00bd36fa90c029595f1092a9b331f9fe
Ivan Aytzhanov <aitvann@gmail.com>
2026-05-18 13:09:45 +0300

    fix(nnn): use app2unit

--- features/wayland.nix
+++ features/wayland.nix
@@ -6,6 +6,21 @@
   util = import ../lib/util.nix args;
   packageHomeFiles = util.packageStowFiles config.home.homeDirectory;
 in {
+  # Will cause massive rebuilds
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     # Source: https://github.com/JManch/nixos/blob/34f070afdbc0e1ec3185e84e8d36fd4fa3e9d716/modules/home-manager/desktop/uwsm.nix#L38
+  #     xdg-utils = prev.xdg-utils.overrideAttrs (old: {
+  #       postFixup =
+  #         (old.postFixup or "")
+  #         + ''
+  #           rm $out/bin/xdg-open
+  #           ln -s ${prev.app2unit}/bin/app2unit-open $out/bin/xdg-open
+  #         '';
+  #     });
+  #   })
+  # ];
+
   home.packages = with pkgs; [
     app2unit
   ];
