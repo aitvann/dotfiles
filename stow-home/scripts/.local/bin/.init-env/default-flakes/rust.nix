3648b8e8101877d5c5b833928a2dda9012df8ea5
Ivan Aytzhanov <aitvann@gmail.com>
2026-06-18 17:47:38 +0300

    fix(flake::rust): nightly

--- stow-home/scripts/.local/bin/.init-env/default-flakes/rust.nix
+++ stow-home/scripts/.local/bin/.init-env/default-flakes/rust.nix
@@ -81,7 +81,7 @@
         packages."${cargoToml.package.name}" = rustPackage "";
 
         devShells.nightly-latest =
-          mkDevShell (pkgs.rust-bin.selectLatestNightlyWith mkToolchain);
+          mkDevShell (pkgs.rust-bin.selectLatestNightlyWith (toolchain: mkToolchain toolchain.default));
         devShells.stable-latest = mkDevShell pkgs.rust-bin.stable.latest.default;
         devShells.msrv = mkDevShell pkgs.rust-bin.stable.${msrv}.default;
         devShells.pinned = mkDevShell pinnedToolchain;
