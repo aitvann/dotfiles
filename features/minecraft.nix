d4e21e50c211b3388871a0971c58a210cb23384b
Ivan Aytzhanov <aitvann@gmail.com>
2026-08-16 01:20:40 +0300

    fix(minecraft): add missing dependency (unfree)

--- features/minecraft.nix
+++ features/minecraft.nix
@@ -3,6 +3,10 @@
   pkgs,
   ...
 }: {
+  imports = [
+    ../modules/unfree.nix
+  ];
+
   nixpkgs.overlays = [
     (final: prev: {
       graalvmPackages21 = inputs.nixpkgs-graalvm21.legacyPackages.${prev.stdenv.hostPlatform.system}.graalvmCEPackages;
