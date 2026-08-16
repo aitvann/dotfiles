00dc9e08c16e6b5a523df7d3d511efd36575f072
Ivan Aytzhanov <aitvann@gmail.com>
2026-08-16 19:36:58 +0300

    feat(init-env::rust): update non-flake version

--- stow-home/dev/.local/bin/.init-env/default-shells/rust.nix
+++ stow-home/dev/.local/bin/.init-env/default-shells/rust.nix
@@ -1,18 +1,60 @@
-{ pkgs ? import <nixpkgs> {
-    overlays = [
-      (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+{
+  pkgs ?
+    import <nixpkgs> {
+      overlays = [
+        (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+      ];
+    },
+}: let
+  runtimeDeps = with pkgs; [];
+  buildDeps = with pkgs; [];
+  devDeps = with pkgs; [
+    # Tools
+    cargo-expand
+    cargo-nextest
+    cargo-all-features
+    cargo-show-asm
+
+    # Editor tools
+    tombi
+  ];
+
+  cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  msrv = cargoToml.package.rust-version;
+
+  toolchainFilePath = ./rust-toolchain.toml;
+  # Legacy variant
+  # toolchainFilePath = ./rust-toolchain;
+  pinnedToolchain = pkgs.rust-bin.fromRustupToolchainFile toolchainFilePath;
+
+  mkToolchain = toolchain: (toolchain.override {
+    extensions = [
+      "rust-src"
+      "rustfmt"
+      "rust-analyzer"
+      "clippy"
     ];
-  }
-}:
-let
-  rustVersion = let
-    toolchain_file = ./rust-toolchain;
-  in
-    if builtins.pathExists toolchain_file
-    then pkgs.rust-bin.fromRustupToolchainFile toolchain_file
-    else pkgs.rust-bin.stable.latest.default;
+  });
+
+  mkDevShell = toolchain:
+    pkgs.mkShell {
+      RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+      buildInputs = runtimeDeps;
+      nativeBuildInputs =
+        buildDeps
+        ++ devDeps
+        ++ [(mkToolchain toolchain)];
+    };
+
+  shell-nightly-latest = mkDevShell (pkgs.rust-bin.selectLatestNightlyWith (toolchain: mkToolchain toolchain.default));
+  shell-stable-latest = mkDevShell pkgs.rust-bin.stable.latest.default;
+  shell-msrv = mkDevShell pkgs.rust-bin.stable.${msrv}.default;
+  shell-pinned = mkDevShell pinnedToolchain;
+  shell-auto =
+    if builtins.pathExists toolchainFilePath
+    then shell-pinned
+    else if cargoToml.package ? rust-version
+    then shell-msrv
+    else shell-stable-latest;
 in
-  pkgs.mkShell {
-    buildInputs =
-      [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-    }
+  shell-auto
