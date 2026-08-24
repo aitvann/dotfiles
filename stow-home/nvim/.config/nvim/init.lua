6dfb1e59ea2be81ce097a163045de6626e7dc859
Ivan Aytzhanov <aitvann@gmail.com>
2026-08-24 15:37:22 +0300

    fix(neovim): allow loading modules by link

--- stow-home/nvim/.config/nvim/init.lua
+++ stow-home/nvim/.config/nvim/init.lua
@@ -39,7 +39,7 @@ require("notifications")
 local modules_dir = require('utils').get_config_root() .. '/lua/modules'
 if vim.uv.fs_stat(modules_dir) then
     for name, type in vim.fs.dir(modules_dir) do
-        if type == 'file' and name:match('%.lua$') then
+        if (type == 'file' or type == 'link') and name:match('%.lua$') then
             local module = 'modules.' .. name:gsub('%.lua$', '')
             local ok, err = pcall(require, module)
             if not ok then
