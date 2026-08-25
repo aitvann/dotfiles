a357fbe0d7ab36a654a9ada8211c9c5011e5de91
Ivan Aytzhanov <aitvann@gmail.com>
2026-08-25 14:04:31 +0300

    refactor(remote-admin): use btop feature

--- features/remote-admin.nix
+++ features/remote-admin.nix
@@ -1,24 +1,18 @@
-{...}: {
-  flake.modules.homeManager.remote-admin = {
-    pkgs,
-    lib,
-    packageHomeFiles,
-    ...
-  }: {
+{inputs, ...}: {
+  flake.modules.homeManager.remote-admin = {pkgs, ...}: {
+    imports = with inputs.self.modules.homeManager; [
+      btop
+    ];
+
     home.packages = with pkgs; [
       git
       stow
-      btop
       # Adding support for Kitty terminal, there might be a better way:
       # - https://sw.kovidgoyal.net/kitty/faq/#i-get-errors-about-the-terminal-being-unknown-or-opening-the-terminal-failing-or-functional-keys-like-arrow-keys-don-t-work
       # - https://sw.kovidgoyal.net/kitty/kittens/ssh/#copying-terminfo-files-manually
       kitty
 
       fastfetch
-    ];
-
-    home.file = lib.mkMerge [
-      (packageHomeFiles "btop")
     ];
   };
 }
