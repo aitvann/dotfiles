5785a7d20ae1d68002083e62a25390aff1a08760
Ivan Aytzhanov <aitvann@gmail.com>
2026-08-25 15:30:09 +0300

    fix(neovim::explorer): verify current location file path

--- features/gaming.nix
+++ features/gaming.nix
@@ -8,7 +8,6 @@
       "steam-unwrapped"
     ];
 
-    # TODO: move to features/gaming.nix
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
--- stow-home/nvim/.config/nvim/lua/explorer.lua
+++ stow-home/nvim/.config/nvim/lua/explorer.lua
@@ -7,5 +7,10 @@ require("nnn").setup({
 })
 
 vim.keymap.set("n", "<leader>e", function()
-    vim.api.nvim_cmd({ cmd = 'NnnPicker', args = { vim.api.nvim_buf_get_name(0) } }, {})
+    local filepath = vim.api.nvim_buf_get_name(0)
+    if filepath ~= "" and vim.fn.filereadable(filepath) == 1 then
+        vim.api.nvim_cmd({ cmd = 'NnnPicker', args = { filepath } }, {})
+    else
+        vim.api.nvim_cmd({ cmd = 'NnnPicker' }, {})
+    end
 end, { silent = true, desc = "open Git coMMits" })
